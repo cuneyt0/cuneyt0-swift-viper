a613ae296dfd89056cba1652d2fe5993191b4f39
Cuneyt AYKAÇ <61650544+cuneyt0@users.noreply.github.com>
2023-11-26 14:46:24 +0300

    Add files via upload

---
+++ protocol_study/protocol_study/home-module/presenter/Presenter.swift
@@ -0,0 +1,11 @@
+//
+//  Presenter.swift
+//  protocol_study
+//
+//  Created by Cuneyt on 23.11.2023.
+//
+
+import Foundation
+class Presenter{
+    
+}
---
+++ protocol_study/protocol_study/home-module/protocols/Protocols.swift
@@ -0,0 +1,9 @@
+//
+//  Protocols.swift
+//  protocol_study
+//
+//  Created by Cuneyt on 23.11.2023.
+//
+
+import Foundation
+
